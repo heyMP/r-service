9c4653cbece9bf241b391e2b1f8c7709c7ed74c1
heyMP <mgp140@gmail.com>
2019-06-18 14:39:21 -0400

    added tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
 var assert = require('assert');
-describe('baseline', function() {
-  describe('example1', function() {
-    it('it should do simple equations', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST || 'http://localhost'
+
+describe('Examples Tests', async function() {
+  it('example1', async function() {
+    const code = fs.readFileSync(path.join(__dirname, 'example1.R'), 'utf8')
+    const res = await axios.post(`${HOST}:${PORT}`, code)
+    return res.data.includes('[1] 4')
   });
 });
